0aba5814d8c380678c500692f221de00d8b49ba0
gusmojsma <gusmojsma@student.gu.se>
2024-11-14 15:33:54 +0100

    #38 Comment decryptor.py

--- server/decryption/src/decryptor.py
+++ server/decryption/src/decryptor.py
@@ -4,21 +4,24 @@ import folder_extraction
 import json_creation
 import copy_latest
 
-# Main function that calls the exttracts data
+# Main function to run the decryption process
 def main():
-
     # Copies all the files from the Moberg Monitor into the data folder
     # copy_latest.copy_files()
 
     # Extract all the data, index, settings and patient files into associated lists
     folder = folder_extraction.FolderExtraction('data/')
-    data_files_list, index_files_list, settings_files_list, patient_info =folder.extract_folders()
 
+    # Extract the data, index, settings and patient information files (binary)
+    data_files_list, index_files_list, settings_files_list, patient_info = folder.extract_folders()
+
+    # Extract the relevant data from the binary files
     decrypter = binary_extraction.DataExtraction(data_files_list, index_files_list, settings_files_list, patient_info)
     
+    # Decrypt the data files (with binary_extraction)
     decrypted_data_files = decrypter.decrypt_all_data_files()
     
+    # Save the decrypted data to json files (which get sent to frontend)
     json_creation.save_to_json(decrypted_data_files, "../../decrypted_data")
-
 
 main()
