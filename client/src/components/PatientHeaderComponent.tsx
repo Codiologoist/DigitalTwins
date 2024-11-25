5f5368420850566381fd1c0d0a792cdb958192e2
Yingchao Ji <gusjiyid@student.gu.se>
2024-11-25 20:16:36 +0100

    #50 Adding selection box and buttons on monitor page

--- client/src/components/PatientHeaderComponent.tsx
+++ client/src/components/PatientHeaderComponent.tsx
@@ -12,6 +12,28 @@ const PatientHeader: React.FC<PatientHeaderProps> = ({patient}) => {
             <h1 className="font-bold ">
                 Patient:  {patient.firstName} {patient.lastName}
             </h1>
+
+            <h1 className="hidden lg:block px-20">|</h1>
+            
+            <div className="flex items-center space-x-2 w-full lg:w-auto">
+                <h1 className="font-bold pr-5">Data Trend:</h1>
+                
+                <div className="flex space-x-5 flex-wrap">
+                    <select
+                        className="bg-black text-white border border-lightgray rounded-md px-3 focus:outline-none focus:ring-2"
+                        aria-label="Select time range"
+                    >
+                        <option value="10m">Last 1 Min</option>
+                        <option value="30m">Last 30 Mins</option>
+                        <option value="1h">Last 1 Hour</option>
+                        <option value="12h">Last 12 Hours</option>
+                    </select>
+                    <button type="button" className="data_trend-button">ECG</button>
+                    <button type="button" className="data_trend-button">ABP</button>
+                    <button type="button" className="data_trend-button">RESP</button>
+                </div>
+            </div>
+
         </div>
     )
 }
