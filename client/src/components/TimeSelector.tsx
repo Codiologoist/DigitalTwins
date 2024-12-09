2fa30ad2ab448be6453b6ab226a167c9f07b73cf
Tehreem Asif <tehreem.asif63@gmail.com>
2024-12-09 16:29:28 +0100

    #57 Adds dropdown with time intervals to show trend view

--- client/src/components/TimeSelector.tsx
+++ client/src/components/TimeSelector.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const TimeSelector: React.FC = () => {
-    const [selectedTime, setSelectedTime] = useState('1min');
+interface TimeSelectorProps {
+    selectedTimeInterval: number;
+    setSelectedTimeInterval: (value: number) => void;
+}
 
+const TimeSelector: React.FC<TimeSelectorProps> = ({ selectedTimeInterval, setSelectedTimeInterval }) => {
     const handleTimeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedTime(event.target.value);
+        setSelectedTimeInterval(Number(event.target.value));
     };
 
     return (
-        <div>
-            <label htmlFor="time-dropdown">Select Time Interval: </label>
-            <select id="time-dropdown" value={selectedTime} onChange={handleTimeChange}>
-                <option value="1min">1min</option>
-                <option value="10min">10min</option>
-                <option value="30min">30min</option>
-                <option value="1hr">1hr</option>
+        <div className="flex items-center">
+            <select
+                id="time-dropdown"
+                value={selectedTimeInterval}
+                onChange={handleTimeChange}
+                className="py-2 px-4 rounded-lg border-2 border-teal-500 bg-teal-100 text-gray-800 text-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
+            >
+                <option value={1}>1min</option>
+                <option value={10}>10min</option>
+                <option value={30}>30min</option>
+                <option value={60}>1hr</option>
             </select>
         </div>
     );
